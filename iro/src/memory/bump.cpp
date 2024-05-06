baec6715c584f9d3371d04a46cb4eac0bfe7f244
sushi <officialaxos7@gmail.com>
2024-05-05 17:56:24 -0700

    iro: add bump allocator

---
+++ iro/src/memory/bump.cpp
@@ -0,0 +1,63 @@
+#include "bump.h"
+#include "memory.h"
+
+namespace iro::mem
+{
+
+b8 Bump::init()
+{
+	allocated_slabs_list = (u8**)mem::stl_allocator.allocate(sizeof(u8**) + slab_size);
+	if (!allocated_slabs_list)
+		return false;
+	start = cursor = (u8*)(allocated_slabs_list + 1);
+	return true;
+}
+
+void Bump::deinit()
+{
+	for (;;)
+	{
+		u8** next = (u8**)*allocated_slabs_list;
+		mem::stl_allocator.free(allocated_slabs_list);
+		if (!next)
+			break;
+		allocated_slabs_list = next;
+	}
+}
+
+void* Bump::allocate(u64 size)
+{
+	assert(size < slab_size);
+
+	// we store the size of the allocation with the data
+	// to support reallocation
+	// idrk if this a good idea
+	u64 total_size = size + sizeof(u64);
+
+	if (cursor - start + total_size > slab_size)
+	{
+		u8* newslab = (u8*)mem::stl_allocator.allocate(sizeof(u8**) + slab_size);
+		*((u8**)start - 1) = newslab;
+		start = cursor = newslab + 1;
+	}
+
+	u64* header = (u64*)cursor;
+	*header = size;
+	u8* out = (u8*)(header + 1);
+	cursor += total_size;
+	return out;
+}
+
+void* Bump::reallocate(void* ptr, u64 new_size)
+{
+	u64 old_size = *((u64*)ptr - 1);
+	if (old_size < new_size)
+	{
+		void* dst = allocate(new_size);
+		mem::copy(dst, ptr, old_size);
+		return dst;
+	}
+	return ptr;
+}
+
+}
---
+++ iro/src/memory/bump.h
@@ -0,0 +1,40 @@
+/*
+ *  Bump allocator, which is pretty much a chunked arena.
+ *
+ *  Allocates memory that we dont intend to cleanup until we're finished with all of it.
+ *  So this does not implement free. In order to support reallocating, though, we do
+ *  still store the size of an allocation before the actual data.
+ */
+
+#ifndef _iro_bump_h
+#define _iro_bump_h
+
+#include "allocator.h"
+#include "assert.h"
+
+namespace iro::mem
+{
+
+struct Bump : public Allocator
+{
+	const static u32 slab_size = 4096;
+
+	u8* start;
+	u8* cursor;
+
+	u8** allocated_slabs_list;
+
+	b8   init();
+	void deinit();
+
+	void* allocate(u64 size) override;
+
+	void* reallocate(void* ptr, u64 size) override;
+	
+	[[deprecated("Bump does not implement free! It is OK to call this function directly, but it will do nothing.")]]
+	void  free(void* ptr) override {}
+};
+
+}
+
+#endif // _iro_bump_h
