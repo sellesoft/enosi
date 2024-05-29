0032449a7fe3ac2ccc820a8347456744417011d2
sushi <officialaxos7@gmail.com>
2024-05-29 12:21:28 -0700

    lpp: build: add dep on lppclang lib and pass flags back to lakefile

--- lpp/lakemodule.lua
+++ lpp/lakemodule.lua
@@ -1,5 +1,7 @@
 local options = assert(lake.getOptions())
 
+local List = require "list"
+
 local mode = options.mode or "debug"
 local build_dir = lake.cwd().."/build/"..mode.."/"
 
@@ -9,16 +11,33 @@ end)
 
 lake.mkdir(build_dir, {make_parents = true})
 
-local report = assert(options.report)
+local report  = assert(options.report)
 local reports = assert(options.reports)
 local recipes = assert(options.recipes)
-
 
 assert(reports.iro.objFiles, "lpp depends on iro's object files but they were not found in the reports table. Was iro's module imported? Maybe it was imported after this one.")
 
 local lpp = lake.target(build_dir.."lpp")
 
 report.executable(tostring(lpp))
+
+local cflags = List
+{
+	"-Isrc",
+	options.getProjIncludeDirFlags "iro"
+}
+
+if mode == "debug" then
+	cflags:push { "-O0", "-ggdb0", "-DLPP_DEBUG=1" }
+else
+	cflags:push "-O2"
+end
+
+local lflags = List
+{
+	options.getProjLibs "luajit",
+	options.getProjLibs "lppclang",
+}
 
 local c_files = lake.find("src/**/*.cpp")
 
@@ -28,21 +47,25 @@ for c_file in c_files:each() do
 
 	report.objFile(o_file)
 
+	lpp:dependsOn(o_file)
+
 	lake.target(o_file)
 		:dependsOn(c_file)
-		:recipe(recipes.compiler(c_file, o_file))
+		:recipe(recipes.compiler(c_file, o_file, cflags))
 
 	lake.target(d_file)
 		:dependsOn(c_file)
-		:recipe(recipes.depfile(c_file, d_file, o_file))
+		:recipe(recipes.depfile(c_file, d_file, o_file, cflags))
 end
 
 local ofiles = lake.flatten { reports.lpp.objFiles, reports.iro.objFiles }
 
-lpp
-	:dependsOn(ofiles)
-	:recipe(recipes.linker(ofiles, lpp))
+lpp:recipe(recipes.linker(ofiles, lpp, lflags))
 
-reports.getProjLibs("luajit"):each(function(e)
+options.getProjLibs("luajit"):each(function(e)
+	lpp:dependsOn(e)
+end)
+
+options.getProjLibs("lppclang"):each(function(e)
 	lpp:dependsOn(e)
 end)
