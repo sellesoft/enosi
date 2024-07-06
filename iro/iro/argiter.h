30a9c3591efe6ee2bb8f7d2d93d2d9c3aa5aaeb7
sushi <officialaxos7@gmail.com>
2024-07-06 15:35:48 -0700

    iro: add argiter helper

---
+++ iro/iro/argiter.h
@@ -0,0 +1,43 @@
+/*
+ *  Small helper for iterating over command line arguments.
+ *
+ */
+
+#ifndef _iro_argiter_h
+#define _iro_argiter_h
+
+#include "common.h"
+#include "unicode.h"
+
+namespace iro
+{
+
+struct ArgIter
+{
+  const char** argv = nullptr;
+  u32 argc = 0;
+  u32 idx = 0;
+
+  str current = nil;
+
+  ArgIter(const char** argv, u32 argc) : argv(argv), argc(argc)
+  {
+    idx = 1;
+    next();
+  }
+
+  void next()
+  {
+    if (idx == argc)
+    {
+      current = nil;
+      return;
+    }
+
+    current = str::fromCStr(argv[idx++]);
+  }
+};
+
+}
+
+#endif // _iro_argiter_h
