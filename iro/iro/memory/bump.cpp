46749c1e6b6a0531673885a7cfe3a4384f1b7e0a
sushi <officialaxos7@gmail.com>
2024-07-17 01:55:27 -0700

    iro: add mem::LenientBump

--- iro/iro/memory/bump.cpp
+++ iro/iro/memory/bump.cpp
@@ -4,6 +4,8 @@
 namespace iro::mem
 {
 
+/* ----------------------------------------------------------------------------
+ */
 b8 Bump::init()
 {
   slabs = (Slab*)mem::stl_allocator.allocate(sizeof(Slab));
@@ -14,6 +16,8 @@ b8 Bump::init()
   return true;
 }
 
+/* ----------------------------------------------------------------------------
+ */
 void Bump::deinit()
 {
   while (slabs)
@@ -24,6 +28,8 @@ void Bump::deinit()
   }
 }
 
+/* ----------------------------------------------------------------------------
+ */
 void* Bump::allocate(u64 size)
 {
   assert(size < slab_size);
@@ -48,6 +54,8 @@ void* Bump::allocate(u64 size)
   return out;
 }
 
+/* ----------------------------------------------------------------------------
+ */
 void* Bump::reallocate(void* ptr, u64 new_size)
 {
   u64 old_size = *((u64*)ptr - 1);
@@ -55,6 +63,69 @@ void* Bump::reallocate(void* ptr, u64 new_size)
   {
     void* dst = allocate(new_size);
     mem::copy(dst, ptr, old_size);
+    return dst;
+  }
+  return ptr;
+}
+
+/* ----------------------------------------------------------------------------
+ */
+b8 LenientBump::init()
+{
+  slabs = (Slab*)mem::stl_allocator.allocate(sizeof(Slab) + slab_size);
+  if (!slabs)
+    return false;
+  slabs->next = nullptr;
+  slabs->size = slab_size;
+  start = cursor = (u8*)slabs + sizeof(Slab);
+  return true;
+}
+
+/* ----------------------------------------------------------------------------
+ */
+void LenientBump::deinit()
+{
+  while (slabs)
+  {
+    Slab* next = slabs->next;
+    mem::stl_allocator.free(slabs);
+    slabs = next;
+  }
+}
+
+/* ----------------------------------------------------------------------------
+ */
+void* LenientBump::allocate(u64 size)
+{
+  u64 total_size = size + sizeof(u64);
+
+  if (cursor - start + total_size > slabs->size)
+  {
+    u64 new_size = total_size > slab_size ? total_size : slab_size;
+    Slab* newslab = 
+      (Slab*)mem::stl_allocator.allocate(sizeof(Slab) + new_size);
+    newslab->next = slabs;
+    slabs = newslab;
+    newslab->size = new_size;
+    start = cursor = (u8*)newslab + sizeof(Slab);
+  }
+
+  u64* header = (u64*)cursor;
+  *header = size;
+  u8* out = (u8*)(header + 1);
+  cursor += total_size;
+  return out;
+}
+
+/* ----------------------------------------------------------------------------
+ */
+void* LenientBump::reallocate(void* ptr, u64 new_size)
+{
+  u64 old_size = *((u64*)ptr - 1);
+  if (old_size < new_size)
+  {
+    void* dst = allocate(new_size);
+    mem::copy(dst,ptr,old_size);
     return dst;
   }
   return ptr;
--- iro/iro/memory/bump.h
+++ iro/iro/memory/bump.h
@@ -1,9 +1,10 @@
 /*
  *  Bump allocator, which is pretty much a chunked arena.
  *
- *  Allocates memory that we dont intend to cleanup until we're finished with all of it.
- *  So this does not implement free. In order to support reallocating, though, we do
- *  still store the size of an allocation before the actual data.
+ *  Allocates memory that we dont intend to cleanup until we're finished with 
+ *  all of it. So this does not implement free. In order to support 
+ *  reallocating, though, we do still store the size of an allocation before 
+ *  the actual data.
  */
 
 #ifndef _iro_bump_h
@@ -15,14 +16,16 @@
 namespace iro::mem
 {
 
+/* ============================================================================
+ */
 struct Bump : public Allocator
 {
   const static u32 slab_size = 4096;
 
   struct Slab
   {
-    Slab* next;
-    u8 content[slab_size];
+    Slab* next = nullptr;
+    u8 content[slab_size] = {};
   };
 
   u8* start;
@@ -37,8 +40,40 @@ struct Bump : public Allocator
 
   void* reallocate(void* ptr, u64 size) override;
   
-  [[deprecated("Bump does not implement free! It is OK to call this function directly, but it will do nothing.")]]
-  void  free(void* ptr) override {}
+  [[deprecated("Bump does not implement free! It is OK to call this function "
+               "directly, but it will do nothing.")]]
+  void free(void* ptr) override {}
+};
+
+/* ============================================================================
+ *  A variant of bump that allocates slabs of size 4096 but will also allocate
+ *  slabs of arbitrary size if necessary. Useful for collecting strings that
+ *  may be really big and what not.
+ */
+struct LenientBump : public Allocator
+{
+  const static u32 slab_size = 4096;
+
+  struct Slab
+  {
+    Slab* next = nullptr;
+    u32 size = 0;
+  };
+
+  u8* start = nullptr;
+  u8* cursor = nullptr;
+
+  Slab* slabs;
+
+  b8   init();
+  void deinit();
+
+  void* allocate(u64 size) override;
+  void* reallocate(void* ptr, u64 size) override;
+
+  [[deprecated("LenientBump does not implement free! It is OK to call this "
+               "function directly, but it will do nothing.")]]
+  void free(void* ptr) override {}
 };
 
 }
