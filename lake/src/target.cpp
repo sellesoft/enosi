93990c6d2087394b9c2921e893bc677abe21c545
sushi <officialaxos7@gmail.com>
2024-09-15 23:28:28 -0400

    lake: use ERROR_)NOFMT when showing recipe errors

--- lake/src/target.cpp
+++ lake/src/target.cpp
@@ -215,7 +215,7 @@ Target::RecipeResult Target::resumeRecipe(LuaState& lua)
   // this explicitly.
   if (!lua.pcall(1, 2))
   {
-    ERROR(lua.tostring(), "\n");
+    ERROR_NOFMT(lua.tostring(), "\n");
     return RecipeResult::Error;
   }
 
@@ -224,7 +224,7 @@ Target::RecipeResult Target::resumeRecipe(LuaState& lua)
   if (!coroutine_success)
   {
     // the second arg is the message given by whatever failure occured
-    ERROR(lua.tostring(), "\n");
+    ERROR_NOFMT(lua.tostring(), "\n");
     return RecipeResult::Error;
   }
 
