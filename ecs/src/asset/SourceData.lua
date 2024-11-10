590e6ce58d5875b822ce63f714dec8ed3001b541
sushi <officialaxos7@gmail.com>
2024-11-10 05:04:58 -0500

    ecs: start on source data stuff

---
+++ ecs/src/asset/SourceData.lua
@@ -0,0 +1,5 @@
+local cmn = require "common"
+
+local SourceData = {}
+
+local Schema = {}
---
+++ ecs/src/common.lua
@@ -0,0 +1,22 @@
+--- 
+--- Common stuff throughout all of ecs.
+---
+
+-- Introduce lpp as a global.
+lpp = require "lpp"
+
+local common = {}
+
+common.reflect = require "reflection.Reflector"
+common.buffer = require "string.buffer"
+
+common.defFileLogger = function(name, verbosity)
+  local buf = common.buffer.new()
+
+  buf:put("static Logger logger = Logger::create(\"", name, 
+          "\"_str, Logger::Verbosity::",verbosity,");")
+
+  return buf:get()
+end
+
+return common
