395be0bb24db6f669de0e9ee93b4225dfe2efa7a
sushi <officialaxos7@gmail.com>
2025-03-11 01:30:40 -0400

    [build] track lpp files in compile_commands.json

--- build/object.lua
+++ build/object.lua
@@ -377,6 +377,7 @@ LppObj.defineTask = function(self, cmd)
   local lpp_cmd, cpp_cmd = cmd:complete(lfile, cfile, ofile, dfile, mfile)
 
   sys.trackCompileCommandsCmd(self.proj.root, cfile, cpp_cmd)
+  sys.trackCompileCommandsCmd(self.proj.root, self.src, lpp_cmd)
 
   local lpp_task = lake.task(lfile)
   local cpp_task = lake.task(cfile)
