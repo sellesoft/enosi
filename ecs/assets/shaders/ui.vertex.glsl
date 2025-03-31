1c5c54b205c5d74339dd3d18f1927f60d96c757a
dellevelled <dellevelled@yahoo.com>
2025-03-31 00:46:18 -0700

    [ecs/graphics] progress on getting the editor and ui rendering (it draws now but with errors)

--- ecs/assets/shaders/ui.vertex.glsl
+++ ecs/assets/shaders/ui.vertex.glsl
@@ -15,7 +15,7 @@ layout(location = 1) out vec4 out_color;
 void main()
 {
   vec4 pos = push.transform * vec4(in_pos, 0.0, 1.0);
-  gl_Position = vec4(pos.xy, 0.0, 1.0);
+  gl_Position = vec4(pos.x - 1.0, pos.y + 1.0, 0.0, 1.0);
   out_uv = in_uv;
   out_color = in_color;
 }
