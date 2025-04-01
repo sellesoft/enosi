f009eff86a81346889b36ea36162bc320b89c99a
dellevelled <dellevelled@yahoo.com>
2025-03-31 21:11:45 -0700

    [ecs/graphics] fix texture colors and uv coordinates

--- ecs/assets/shaders/ui.vertex.glsl
+++ ecs/assets/shaders/ui.vertex.glsl
@@ -15,7 +15,8 @@ layout(location = 1) out vec4 out_color;
 void main()
 {
   vec4 pos = push.transform * vec4(in_pos, 0.0, 1.0);
-  gl_Position = vec4(pos.x - 1.0, pos.y + 1.0, 0.0, 1.0);
+  //TODO(delle) encode this subtraction into the transform matrix
+  gl_Position = vec4(pos.x - 1.0, pos.y - 1.0, 0.0, 1.0);
   out_uv = in_uv;
   out_color = in_color;
 }
