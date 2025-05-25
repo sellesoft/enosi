082b079a352fb3141efc19609d7a74ab365bad00
sushi <officialaxos7@gmail.com>
2025-05-24 20:07:27 -0400

    [lpp] handle errors in the error handler

--- lpp/src/Errh.lua
+++ lpp/src/Errh.lua
@@ -3,27 +3,37 @@ local lpp = require "Lpp"
 return function(errmsg)
   if errmsg == lpp.cancel then return lpp.cancel end 
 
-  local stack = {}
-  local fidx = 2
-  while true do
-    local info = debug.getinfo(fidx)
-    if not info then break end
-    if info.what ~= "C" and not lpp.stacktrace_func_filter[info.func] then 
-      table.insert(stack, 1,
-        {
-          src = info.source,
-          line = info.currentline,
-          name = lpp.stacktrace_func_rename[info.func] or info.name,
-          metaenv = getfenv(info.func).__metaenv,
-        })
+  local result = {pcall(function()
+
+    local stack = {}
+    local fidx = 2
+    while true do
+      local info = debug.getinfo(fidx)
+      if not info then break end
+      if info.what ~= "C" and not lpp.stacktrace_func_filter[info.func] then 
+        table.insert(stack, 1,
+          {
+            src = info.source,
+            line = info.currentline,
+            name = lpp.stacktrace_func_rename[info.func] or info.name,
+            metaenv = getfenv(info.func).__metaenv,
+          })
+      end
+      fidx = fidx + 1
     end
-    fidx = fidx + 1
+
+    return 
+    {
+      stack = stack,
+      msg = errmsg:gsub("%[.-%]:%d-: ", "")
+                  :gsub(".-:%d-: ", "")
+    }
+  end)}
+
+  if result[1] then
+    return result[2]
   end
 
-  return
-  {
-    stack = stack,
-    msg = errmsg:gsub("%[.-%]:%d-: ", "")
-                :gsub(".-:%d-: ", "")
-  }
+  print(result[2])
+    
 end
